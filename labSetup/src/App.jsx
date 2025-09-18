7aa65befffb7fcf86567e5e83c4ff7c5fd800475
ryanJackman05 <20108256@mail.wit.ie>
2025-09-18 11:53:32 +0100

    Lab 1 save1

--- labSetup/src/App.jsx
+++ labSetup/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Task from './components/Task';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>Lab 1</h1>
+          <Task title = "Gaming" deadline = "ASAP"> Do some gaming</Task>
+          <Task title = "Tidy" deadline = "Tomorrow or the day after idk"> Clean up the house</Task>
+          <Task title = "Dishes" deadline = "When I feel like it"> Do the dishes</Task>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ labSetup/src/components/Task.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Task = (props) => {
+    
+    return (
+        <div class="card">
+            <p class="title">{props.title} </p>
+            <p>Due {props.deadline} </p>
+            <p>{props.children}</p>
+        </div>
+        
+    )
+}
+
+export default Task;
