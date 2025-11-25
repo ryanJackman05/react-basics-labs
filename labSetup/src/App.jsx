338149681de9f2245be5ddd3dfbfb4f0180e5454
ryanJackman05 <20108256@mail.wit.ie>
2025-11-25 12:17:14 +0000

    Add task functionality

--- labSetup/src/App.jsx
+++ labSetup/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
 import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
@@ -12,8 +13,33 @@ function App() {
       { id:3, title: "Tidy up", deadline: "Today", priority:"Low", done:false}
     ]
   });
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
+
+  const formChangeHandler = (event) => {
+    let form = {...formState}; // spread operator makes a copy
+
+    switch(event.target.name) { // change field depending on target component from event
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+    console.log(formState);
+  }
   const doneHandler = (taskIndex) => {
-    const tasks = [...taskState.tasks]; // spread operator reads taskState.tasks as a whole array
+    const tasks = [...taskState.tasks]; // spread operator reads and copies the array object, rather than a reference.
     tasks[taskIndex].done = !tasks[taskIndex].done; // flip bool state of the one task
     setTaskState({tasks});
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
@@ -22,7 +48,18 @@ function App() {
     const tasks = [...taskState.tasks]; // spread operator reads taskState.tasks as a whole array
     tasks.splice(taskIndex, 1);
     setTaskState({tasks}); // assign new tasks object to taskState
-  } 
+  }
+  const formSubmitHandler = (event) => { // event parameter -> refer to Form.jsx form
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks]; // new tasks duplicate
+    const form = {...formState}; // new form duplicate
+
+    form.id = uuidv4();
+    
+    tasks.push(form); // add contents of current form to the front of tasks array.
+    setTaskState({tasks}); // set new tasks
+  }
 
   return (
     <div className="container">
@@ -39,7 +76,7 @@ function App() {
         deleteTask = {() => deleteHandler(index)}
         />
       ))}
-      <AddTaskForm />
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />/*pass the handler in*/
 
     </div>
   );
